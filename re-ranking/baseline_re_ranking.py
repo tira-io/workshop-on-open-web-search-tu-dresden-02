c85e6bd6ce1b68d18e3be9d4ce2a5a118af2be2f
Per Natzschka <rep.akhcsztan@gmail.com>
2024-03-04 13:37:40 +0000

    output definition (by Maik)

--- re-ranking/baseline_re_ranking.py
+++ re-ranking/baseline_re_ranking.py
@@ -4,9 +4,9 @@ from tira.third_party_integrations import load_rerank_data, persist_and_normaliz
 from pathlib import Path
 import pandas as pd
 
-def score_query_document_pair(query, document_text, score_of_previous_stage):
+def find_top_snippets(query, document_text):
     # Our baseline re-ranker just emits 1 + the score of the previous ranker.
-    return 1 + score_of_previous_stage
+    return [{'snippet_score':1, 'snippet_text': 'ddsfds'}, {'snippet_score':10.9, 'snippet_text': 'a'}, ]
 
 if __name__ == '__main__':
     # In the TIRA sandbox, this is the injected re-ranking dataset, injected via the environment variable TIRA_INPUT_DIRECTORY
@@ -16,15 +16,10 @@ if __name__ == '__main__':
     # from tira.third_party_integrations import ir_dataset
     # dataset = ir_datasets.load(default='workshop-on-open-web-search/document-processing-20231027-training')
 
-    run = []
+    document_snippets = []
 
     for _, i in re_rank_dataset.iterrows():
-        run += [{'qid': i['qid'], 'docno': i['docno'], 'score': score_query_document_pair(i['query'], i['text'], i['score'])}]
+        document_snippets += [{'qid': i['qid'], 'docno': i['docno'], 'snippets': find_top_snippets(i['query'], i['text'])}]
     
-    run = pd.DataFrame(run)
-    
-    # Re-rankers are expected to produce a TREC-style run.txt file in the output directory.
-    persist_and_normalize_run(run, 'my-system-name', default_output='./run.txt')
-    
-    # You can pass additional arguments to your program, e.g., via argparse, to modify the behaviour of your program.
-    
+    document_snippets = pd.DataFrame(document_snippets)
+    document_snippets.to_json('./re-rank.jsonl.gz', lines=True, orient='records')
