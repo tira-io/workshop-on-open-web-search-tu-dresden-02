e90266d05d6577281176a955eab186fbb6c360b8
Per Natzschka <rep.akhcsztan@gmail.com>
2024-03-04 14:03:10 +0000

    added base functions

--- re-ranking/baseline_re_ranking.py
+++ re-ranking/baseline_re_ranking.py
@@ -4,16 +4,32 @@ from tira.third_party_integrations import load_rerank_data, persist_and_normaliz
 from pathlib import Path
 import pandas as pd
 
+def split_into_snippets(document_text):
+    pass
+
+def transform_snippet_format(snippets):
+    pass
+
+def rank_snippets(query, snippets):
+    pass
+
 def find_top_snippets(query, document_text):
     # First: split document_text into snippets
     # https://github.com/grill-lab/trec-cast-tools/tree/master/corpus_processing/passage_chunkers
+    
+    snippets = split_into_snippets(document_text)
 
+    # Second: transform snippet format from output of split_into_snippets to input of rank_snippets
 
-    # Second: rank snippets 
+    snippets = transform_snippet_format(snippets)
 
+    # Third: rank snippets
+
+    ranking = rank_snippets(query, snippets)
 
     # Return values
-    return [{'snippet_score':1, 'snippet_text': 'ddsfds'}, {'snippet_score':10.9, 'snippet_text': 'a'}, ]
+    return ranking
+    #return [{'snippet_score':1, 'snippet_text': 'ddsfds'}, {'snippet_score':10.9, 'snippet_text': 'a'}, ]
 
 if __name__ == '__main__':
     # In the TIRA sandbox, this is the injected re-ranking dataset, injected via the environment variable TIRA_INPUT_DIRECTORY
