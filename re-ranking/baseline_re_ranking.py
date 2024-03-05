afe01009626a358e42cd52ae641adc0219a3fa92
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-03-05 09:06:13 +0000

    reformatted baseline_re_ranking.py

--- re-ranking/baseline_re_ranking.py
+++ re-ranking/baseline_re_ranking.py
@@ -7,39 +7,43 @@ import os
 import shutil
 import pandas as pd
 import pyterrier as pt
+
 if not pt.started():
-  pt.init()
+    pt.init()
+
 
 def split_into_snippets(document_text):
     chunker = spacy_passage_chunker.SpacyPassageChunker()
     return chunker.process_batch([{
-            "id" : 0,
-            "url": '',
-            "title" : '',
-            "contents" : document_text
-        }])[0]['contents']
+        "id": 0,
+        "url": '',
+        "title": '',
+        "contents": document_text
+    }])[0]['contents']
+
 
 def transform_snippet_format(snippets):
     df = pd.DataFrame({
-                    'docno': [str(snippet['id']) for snippet in snippets],
-                    'text': [snippet['body'] for snippet in snippets]
-                    })
+        'docno': [str(snippet['id']) for snippet in snippets],
+        'text': [snippet['body'] for snippet in snippets]
+    })
     return df
+
 
 def rank_snippets(query, snippets_df):
     if os.path.exists('pd_index'):
-    # Remove the directory and all its contents
+        # Remove the directory and all its contents
         shutil.rmtree('pd_index')
     pd_indexer = pt.DFIndexer("./pd_index")
     indexref3 = pd_indexer.index(snippets_df["text"], snippets_df["docno"])
     index = pt.IndexFactory.of(indexref3)
-    bm25 = pt.BatchRetrieve(index, controls = {"wmodel": "BM25"})
+    bm25 = pt.BatchRetrieve(index, controls={"wmodel": "BM25"})
     #print(query)
 
     #remove ? due to error in terrier query parser
     query = query.replace('?', '')
     if os.path.exists('pd_index'):
-    # Remove the directory and all its contents
+        # Remove the directory and all its contents
         shutil.rmtree('pd_index')
 
     result = bm25.search(query)
@@ -48,10 +52,11 @@ def rank_snippets(query, snippets_df):
 
     return result
 
+
 def find_top_snippets(query, document_text):
     # First: split document_text into snippets
     # https://github.com/grill-lab/trec-cast-tools/tree/master/corpus_processing/passage_chunkers
-    
+
     snippets = split_into_snippets(document_text)
 
     # Second: transform snippet format from output of split_into_snippets to input of rank_snippets
@@ -64,7 +69,8 @@ def find_top_snippets(query, document_text):
 
     # Return values
     return ranking
-    
+
+
 if __name__ == '__main__':
     # In the TIRA sandbox, this is the injected re-ranking dataset, injected via the environment variable TIRA_INPUT_DIRECTORY
     re_rank_dataset = load_rerank_data(default='workshop-on-open-web-search/re-ranking-20231027-training')
@@ -76,7 +82,8 @@ if __name__ == '__main__':
     document_snippets = []
 
     for _, i in re_rank_dataset.iterrows():
-        document_snippets += [{'qid': i['qid'], 'docno': i['docno'], 'snippets': find_top_snippets(i['query'], i['text'])}]
-    
+        document_snippets += [
+            {'qid': i['qid'], 'docno': i['docno'], 'snippets': find_top_snippets(i['query'], i['text'])}]
+
     document_snippets = pd.DataFrame(document_snippets)
     document_snippets.to_json('./re-rank.jsonl.gz', lines=True, orient='records')
