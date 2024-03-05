bb2faee26b99a85ef390def4a18fe482c97be798
Per Natzschka <rep.akhcsztan@gmail.com>
2024-03-05 17:14:18 +0000

    added test cases for longer documents

---
+++ test/test_snippet_generation_end_to_end.py
@@ -0,0 +1,73 @@
+import unittest
+from approvaltests import verify_as_json
+import json
+from re_ranking.baseline_re_ranking import find_top_snippets
+
+
+class EndToEndSnippetGeneratorTest(unittest.TestCase):
+    def test_top_snippets_example_01_pl2(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-01.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'PL2', 3)
+        verify_as_json(actual)
+
+    def test_top_snippets_example_01_tf(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-01.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'Tf', 3)
+        verify_as_json(actual) 
+
+
+    def test_top_snippets_example_02_pl2(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-02.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'PL2', 30)
+        verify_as_json(actual)
+
+
+    def test_top_snippets_example_02_tf(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-02.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'Tf', 30)
+        verify_as_json(actual)
+
+
+
+    def test_top_snippets_example_03_pl2(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-03.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'PL2', 30)
+        verify_as_json(actual)
+
+
+    def test_top_snippets_example_03_tf(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-03.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'Tf', 30)
+        verify_as_json(actual)
+
+
+    def test_top_snippets_example_04_pl2(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-04.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'PL2', 30)
+        verify_as_json(actual)
+
+
+    def test_top_snippets_example_04_tf(self):
+        query_doc_pair = json.load(open('test/test-rerank-example-04.json'))
+        document = query_doc_pair['text']
+        query = query_doc_pair['query']
+        actual = find_top_snippets(query, document, 'Tf', 30)
+        verify_as_json(actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
