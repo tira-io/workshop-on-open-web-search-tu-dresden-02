05d1ceba9ac58716928acb764f48027786e24796
Maik Fröbe <maik.froebe@uni-jena.de>
2024-03-06 22:18:39 +0100

    unify files to fix import error

--- src/snippet_generation.py
+++ src/snippet_generation.py
@@ -13,7 +13,6 @@ import pyterrier as pt
 import pyterrier_dr
 
 from sentence_transformers import CrossEncoder
-from src.parameterized_spacy_passage_chunker import ParameterizedSpacyPassageChunker
 # Load a patched ir_datasets that loads the injected data inside the TIRA sandbox
 from tira.third_party_integrations import load_rerank_data, ensure_pyterrier_is_loaded, get_output_directory
 
@@ -22,6 +21,52 @@ ensure_pyterrier_is_loaded()
 tokeniser = pt.autoclass("org.terrier.indexing.tokenisation.Tokeniser").getTokeniser()
 def pt_tokenise(text):
     return ' '.join(tokeniser.getTokens(text))
+
+# adapted from passage_chunkers.spacy_passage_chunker
+# https://github.com/grill-lab/trec-cast-tools/blob/master/corpus_processing/passage_chunkers/spacy_passage_chunker.py
+import re
+
+import spacy
+from tqdm import tqdm
+
+from passage_chunkers.abstract_passage_chunker import AbstractPassageChunker
+
+nlp = spacy.load("en_core_web_sm", exclude=[
+    "parser", "tagger", "ner", "attribute_ruler", "lemmatizer", "tok2vec"])
+nlp.enable_pipe("senter")
+nlp.max_length = 2000000  # for documents that are longer than the spacy character limit
+
+
+class ParameterizedSpacyPassageChunker(AbstractPassageChunker):
+    """
+    Basically the same as #SpacyPassageChunker. Only difference is that the snippet size can be set in #__init__
+    """
+    def __init__(self, snippet_size=250):
+        self.snippet_size = snippet_size
+
+    def process_batch(self, document_batch: list[dict]) -> list[dict]:
+        """
+        Divides the documents of the given document list into snippets.
+        :param document_batch: List of documents. Documents are dicts - content must be at 'contents' key.
+        :return: List of documents. Documents are dicts - snippets can be found at 'contents' key.
+        """
+        regexp = re.compile(r'[a-zA-Z0-9]')
+        filter(lambda doc: regexp.search(doc['contents']), document_batch)
+        batch_document_texts = [document['contents'] for document in document_batch]
+        processed_document_texts = nlp.pipe(batch_document_texts, n_process=1)
+
+        for index, document in tqdm(enumerate(processed_document_texts), total=len(document_batch)):
+            document_sentences = list(document.sents)
+            sentences_word_count = [
+                len([token for token in sentence])
+                for sentence in document_sentences
+            ]
+
+            generated_passages = self.chunk_document(document_sentences, sentences_word_count, self.snippet_size)
+            document_batch[index]['contents'] = generated_passages
+
+        return document_batch
+
 
 def split_dataframe_into_snippets(documents: pd.DataFrame, snippet_size=250) -> pd.DataFrame:
     document_list = documents.rename(columns={'text': 'contents'}).to_dict('records')
