5f74b59326675d533b60fc085fe65f551ffea979
Per Natzschka <rep.akhcsztan@gmail.com>
2024-03-06 12:20:11 +0000

    added test_snippet_generation_big_data.py

---
+++ test/test_snippet_generation_big_data.py
@@ -0,0 +1,18 @@
+import unittest
+import json
+import gzip
+from src.snippet_generation import find_top_snippets
+
+
+class EndToEndSnippetGeneratorTest(unittest.TestCase):
+    def test_top_snippets_example_05_pl2(self):
+        example_file = open('test/test-rerank-example-05.json', 'r')
+        print('read complete')
+        
+        for line in example_file.readlines():
+            data = json.loads(line)
+            find_top_snippets(data['query'], data['text'], ranker='PL2', max_snippets=3, use_crossencoder=False)
+
+
+if __name__ == '__main__':
+    unittest.main()
