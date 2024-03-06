297d6568ec1c6ea0d6d712c29d097b4cd21698ba
Ai4dse <45199720+Ai4dse@users.noreply.github.com>
2024-03-06 12:46:13 +0000

    added bigdata tests for Bm25 and Tf

--- test/test_snippet_generation_big_data.py
+++ test/test_snippet_generation_big_data.py
@@ -13,6 +13,22 @@ class EndToEndSnippetGeneratorTest(unittest.TestCase):
             data = json.loads(line)
             find_top_snippets(data['query'], data['text'], ranker='PL2', max_snippets=3, use_crossencoder=False)
 
+    def test_top_snippets_example_05_bm25(self):
+        example_file = open('test/test-rerank-example-05.json', 'r')
+        print('read complete')
+        
+        for line in example_file.readlines():
+            data = json.loads(line)
+            find_top_snippets(data['query'], data['text'], ranker='BM25', max_snippets=3, use_crossencoder=False)
+
+    def test_top_snippets_example_05_tf(self):
+        example_file = open('test/test-rerank-example-05.json', 'r')
+        print('read complete')
+        
+        for line in example_file.readlines():
+            data = json.loads(line)
+            find_top_snippets(data['query'], data['text'], ranker='Tf', max_snippets=3, use_crossencoder=False)
+
 
 if __name__ == '__main__':
     unittest.main()
