0f95bedfe70c3ab0620b31d0a7cb6f49c48e138f
Per Natzschka <rep.akhcsztan@gmail.com>
2024-03-07 14:33:39 +0000

    added tests for example 02

--- test/test_snippet_generation_end_to_end.py
+++ test/test_snippet_generation_end_to_end.py
@@ -21,58 +21,42 @@ class EndToEndSnippetGeneratorTest(unittest.TestCase):
         actual = split_dataframe_into_snippets(documents)
         verify_as_json(actual)
 
-    def test_top_snippets_example_02_pl2(self):
-        data = pd.read_json('test/test-rerank-example-02.json', lines=True)
+    def run_sample(self, path_to_data: str, wmodel: str, cross_encode: bool) -> list[dict]:
+        data = pd.read_json(path_to_data, lines=True)
         preprocessed_docs = split_dataframe_into_snippets(data)
         snippets = []
         for qid, i in preprocessed_docs.items():
-            snippets += find_top_snippets_for_all_documents(qid, i['query'], i['documents'], wmodel='PL2', cross_encode=False)
-        verify_as_json(snippets)
+            snippets += find_top_snippets_for_all_documents(qid, i['query'], i['documents'], wmodel=wmodel, cross_encode=cross_encode)
+        return snippets
+
+
+    def test_top_snippets_example_02_pl2(self):
+        actual = self.run_sample('test/test-rerank-example-02.json', wmodel='PL2', cross_encode=False)
+        verify_as_json(actual)
 
 
     def test_top_snippets_example_02_tf(self):
-        query_doc_pair = json.load(open('test/test-rerank-example-02.json'))
-        document = query_doc_pair['text']
-        query = query_doc_pair['query']
-        qid = query_doc_pair['qid']
-        actual = find_top_snippets_for_all_documents(qid, query, document, ranker='Tf', use_crossencoder=False)
+        actual = self.run_sample('test/test-rerank-example-02.json', wmodel='Tf', cross_encode=False)
         verify_as_json(actual)
 
 
-
-    def test_top_snippets_example_03_pl2(self):
-        query_doc_pair = json.load(open('test/test-rerank-example-03.json'))
-        document = query_doc_pair['text']
-        query = query_doc_pair['query']
-        qid = query_doc_pair['qid']
-        actual = find_top_snippets_for_all_documents(qid, query, document, ranker='PL2', max_snippets=30, use_crossencoder=False)
+    def test_top_snippets_example_02_bm25(self):
+        actual = self.run_sample('test/test-rerank-example-02.json', wmodel='BM25', cross_encode=False)
         verify_as_json(actual)
 
 
-    def test_top_snippets_example_03_tf(self):
-        query_doc_pair = json.load(open('test/test-rerank-example-03.json'))
-        document = query_doc_pair['text']
-        query = query_doc_pair['query']
-        qid = query_doc_pair['qid']
-        actual = find_top_snippets_for_all_documents(qid, query, document, ranker='Tf', max_snippets=30, use_crossencoder=False)
+    def test_top_snippets_example_02_pl2_cross_encoder(self):
+        actual = self.run_sample('test/test-rerank-example-02.json', wmodel='PL2', cross_encode=True)
         verify_as_json(actual)
 
 
-    def test_top_snippets_example_04_pl2(self):
-        query_doc_pair = json.load(open('test/test-rerank-example-04.json'))
-        document = query_doc_pair['text']
-        query = query_doc_pair['query']
-        qid = query_doc_pair['qid']
-        actual = find_top_snippets_for_all_documents(qid, query, document, ranker='PL2', max_snippets=30, use_crossencoder=False)
+    def test_top_snippets_example_02_tf_cross_encoder(self):
+        actual = self.run_sample('test/test-rerank-example-02.json', wmodel='Tf', cross_encode=True)
         verify_as_json(actual)
 
 
-    def test_top_snippets_example_04_tf(self):
-        query_doc_pair = json.load(open('test/test-rerank-example-04.json'))
-        document = query_doc_pair['text']
-        query = query_doc_pair['query']
-        qid = query_doc_pair['qid']
-        actual = find_top_snippets_for_all_documents(qid, query, document, ranker='Tf', max_snippets=30, use_crossencoder=False)
+    def test_top_snippets_example_02_bm25_cross_encoder(self):
+        actual = self.run_sample('test/test-rerank-example-02.json', wmodel='BM25', cross_encode=True)
         verify_as_json(actual)
 
 
