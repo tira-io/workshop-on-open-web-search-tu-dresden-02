0756271a629a41c2093b283cb1820d0853e7ea8a
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-03-11 14:51:44 +0000

    added code documentation

--- src/snippet_generation.py
+++ src/snippet_generation.py
@@ -64,6 +64,16 @@ class ParameterizedSpacyPassageChunker(AbstractPassageChunker):
 
 def split_dataframe_into_snippets(documents: pd.DataFrame, snippet_size: int = 250) -> (
         dict)[str, dict[str, str | dict[str, list[dict[str, int | str]]]]]:
+    """
+    Splits the documents in the given DataFrame into snippets of the given token length.
+    @param documents: DataFrame of query-document pairs with rows 'qid' (i.e., the query id), 'query'
+    (i.e., the query string), 'docno' (i.e., the document id) and 'text' (i.e., the document text).
+    @param snippet_size: Approximate size of the generated snippets. Actual size may be lower.
+    @return: A dict mapping query ids to query dicts.
+    Query dicts contain the keys 'query' (i.e., the query string) and a document dict.
+    A document dict maps document numbers (docno) to the respective list of snippets.
+    Snippets are also dicts and contain the keys 'id' and 'body'.
+    """
     document_list = documents.rename(columns={'text': 'contents'}).to_dict('records')
 
     chunker = ParameterizedSpacyPassageChunker(snippet_size)
@@ -80,6 +90,24 @@ def split_dataframe_into_snippets(documents: pd.DataFrame, snippet_size: int = 2
 
 def cross_encode(document_snippet_list: list[dict[str, str | dict[str, float | str]]], model, tokenizer) -> (
         list)[dict[str, str | dict[str, float | str]]]:
+    """
+    Re-ranks the given snippets using the given cross-encoder model and tokenizer.
+    @param document_snippet_list: List of dicts, each representing a document-query pair.
+    A document-query dict contains keys 'qid' which is the id of the query, 'query' which is the actual query string
+    query, 'docno' which is the id of the respective document and 'snippets' which is a list of dicts that each
+    represent a snippet.
+    A snippet is a dict containing keys 'wmodel' which is a str defining the weighting model that calculated  the score,
+    'score' which is a floating point scoring value and 'text' which is the actual text content of the snippet.
+    @param model: Cross-encoder model (should not be None when do_cross_encode is true).
+    @param tokenizer: Cross-encoder tokenizer (should not be None when do_cross_encode is true).
+    @return: A list of dicts, each representing a document-query pair.
+    A document-query dict contains keys 'qid' which is the id of the query, 'query' which is the actual query string
+    query, 'docno' which is the id of the respective document and 'snippets' which is a list of dicts that each
+    represent a snippet.
+    A snippet is a dict containing keys 'wmodel' which is a str defining the weighting model that calculated the score
+    before the cross-encoder, 'score' which is a floating point scoring value and 'text' which is
+    the actual text content of the snippet.
+    """
     pairs = []
     for document in document_snippet_list:
         for snippet in document['snippets']:
@@ -104,7 +132,25 @@ def cross_encode(document_snippet_list: list[dict[str, str | dict[str, float | s
 
 def find_top_snippets_for_all_documents(query_id: str, query: str, documents: dict[str, list[dict[str, str]]],
                                         wmodel: str, top_snippets: int, do_cross_encode: bool, model=None,
-                                        tokenizer=None) -> list[dict[str, str | dict[str, float | str]]]:
+                                        tokenizer=None) -> list[dict[str, str | list[dict[str, float | str]]]]:
+    """
+    Ranks the given snippets as described in README
+    @param query_id: Id of the respective query.
+    @param query: String that has been searched.
+    @param documents: Dict, that maps document numbers (docno) to the respective list of snippets.
+    Snippets are also dicts and must contain the keys 'id' and 'body'.
+    @param wmodel: Str defining the weighting model used for pre-ranking (either 'BM25', 'PL2' or 'Tf').
+    @param top_snippets: Int defining the number k of top snippets (see README).
+    @param do_cross_encode: Bool defining whether step 4 (see README) should be included.
+    @param model: Cross-encoder model (should not be None when do_cross_encode is true).
+    @param tokenizer: Cross-encoder tokenizer (should not be None when do_cross_encode is true).
+    @return: A list of dicts, each representing a document-query pair.
+    A document-query dict contains keys 'qid' which is equal to parameter query_id, 'query' which is equal to parameter
+    query, 'docno' which is the id of the respective document and 'snippets' which is a list of dicts that each
+    represent a snippet.
+    A snippet is a dict containing keys 'wmodel' which is a str equal to parameter wmodel, 'score' which is a floating
+    point scoring value and 'text' which is the actual text content of the snippet.
+    """
     # tokenise query
     query = ' '.join(tokeniser.getTokens(query))
     df = []
@@ -134,7 +180,7 @@ def find_top_snippets_for_all_documents(query_id: str, query: str, documents: di
         ret += [{'qid': query_id,
                  'query': query,
                  'docno': docno,
-                 'snippets': sorted(snippets, key=lambda j: j['score'], reverse=True)[:3]
+                 'snippets': sorted(snippets, key=lambda j: j['score'], reverse=True)[:top_snippets]
                  }]
     if do_cross_encode and model and tokenizer:
         cross_encode(ret, model, tokenizer)
@@ -142,13 +188,24 @@ def find_top_snippets_for_all_documents(query_id: str, query: str, documents: di
 
 
 def parse_arguments() -> argparse.Namespace:
+    """
+    Parses the given command line argument.
+    @return: An argparse.Namespace containing the respective values.
+    """
     parser = argparse.ArgumentParser(description="Argument Parser")
 
+    # select retrieval model - BM25, PL2 or Tf (default)
     parser.add_argument("--retrieval", choices=["BM25", "PL2", "Tf"], default="Tf", help="The retrieval model")
+
+    # use cross-encoder (default false)
     parser.add_argument("--cross-encode", action="store_true", default=False, help="Use a cross-encoder to re-rank "
                                                                                    "the top-k passages of the "
                                                                                    "retrieval model")
+
+    # select snippet size in tokens (default 250)
     parser.add_argument("--snippet-size", default=250, type=int, help="The approximate size of created snippets")
+
+    # select number of top snippets to
     parser.add_argument("--top-snippets", default=3, type=int, help="Number k for top k snippets that are retrieved.")
 
     return parser.parse_args()
@@ -163,11 +220,15 @@ if __name__ == '__main__':
     # from tira.third_party_integrations import ir_dataset
     # re_rank_dataset = ir_datasets.load(default='workshop-on-open-web-search/document-processing-20231027-training')
 
+    # get command line arguments
     args = parse_arguments()
+
+    # split docs into snippets (see step 1 in README)
     preprocessed_docs = split_dataframe_into_snippets(re_rank_dataset, args.snippet_size)
 
     document_snippets = []
 
+    # initialize cross-encoder if requested
     ce_model, ce_tokenizer = None, None
     if args.cross_encode:
         print('Loading cross-encoder model')
@@ -175,6 +236,7 @@ if __name__ == '__main__':
         ce_tokenizer = AutoTokenizer.from_pretrained('cross-encoder/ms-marco-MiniLM-L-6-v2')
         print('Done cross-encoder model is loaded.')
 
+    # perform re-ranking for all queries
     for qid, doc in tqdm(preprocessed_docs.items()):
         document_snippets += find_top_snippets_for_all_documents(qid, doc['query'], doc['documents'], args.retrieval,
                                                                  args.top_snippets, args.cross_encode, model=ce_model,
